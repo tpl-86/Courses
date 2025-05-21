09bad3d741910068823669ebc7d1fe7e3329c3f8
tpl-86 <tron-050686@mail.ru>
2025-05-21 08:52:42 +0300

    Added models shop

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from dgango.utils import timezone
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length = 255)
+    created_at = madels.DateTimeField(default=timezone.now)
+
+class Course(models.Model):
+    title = models.CharField(max_length = 255)
+    price = models.FloatField()
+    students_qty = models.IntegerField()
+    reviews_qty = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = madels.DateTimeField(default=timezone.now)
+
